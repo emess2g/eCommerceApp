c3babcdcbfb380f53c9ad8ec5281910b10e720a3
emess2g <essuonemma84gmail.com>
2024-10-21 14:40:43 +0000

    defined about

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import {assets} from '../assets/assets'
 const Navbar = () => {
   return (
-    <div>
+    <div className='flex items-center justify-between py-5 font-medium'>
       
+      <img src={assets.logo} className='w-36' alt="" />
     </div>
   )
 }
