61446f3f877f2da2b5a32ee78d4b6a113aa1b839
emess2g <essuonemma84gmail.com>
2024-10-22 05:16:28 +0000

    added more policy

--- src/components/OurPolicy.jsx
+++ src/components/OurPolicy.jsx
@@ -5,10 +5,20 @@ const OurPolicy = () => {
   return (
     <div className='flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 text-center py-20 text-sm sm:text-sm md:text-base text-gray-700'>
       
-      <div className="">
+      <div>
         <img src={assets.exchange_icon} className='w-12 m-auto mb-5' alt="" />
         <p className='font-medium'>Easy Exchange Policy</p>
         <p className='text-gray-400'>We offer hassel free exhange policy</p>
+      </div>
+      <div>
+        <img src={assets.quality_icon} className='w-12 m-auto mb-5' alt="" />
+        <p className='font-medium'>7 Days Return Policy</p>
+        <p className='text-gray-400'>We provide 7 days free return policy</p>
+      </div>
+      <div>
+        <img src={assets.support_img} className='w-12 m-auto mb-5' alt="" />
+        <p className='font-medium'>Best Customer Support</p>
+        <p className='text-gray-400'>We provide 24/7 customer support </p>
       </div>
     </div>
   )
