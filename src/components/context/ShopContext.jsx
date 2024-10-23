5add58d1371219371176db02875e02a92e9d1bde
emess2g <essuonemma84gmail.com>
2024-10-23 08:06:33 +0000

    search variables

--- src/components/context/ShopContext.jsx
+++ src/components/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../../assets/assets";
 
 export const ShopContext = createContext();
@@ -7,12 +7,16 @@ const ShopContextProvider = (props) => {
 
     const currency = '$';
     const delivery_fee = 10;
+    const [search,setSearch] = useState('');
+    const [showShow,setShowSearch] = useState(true);
 
 
     const value = {
        products,
        currency,
-       delivery_fee
+       delivery_fee,
+       search,setSearch,
+       showShow,setShowSearch
     }
 
     return (
