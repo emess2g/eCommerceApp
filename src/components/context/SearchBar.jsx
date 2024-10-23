9d32d95eaf8a3152fd7852fe9a12daf9fea46007
emess2g <essuonemma84gmail.com>
2024-10-23 08:10:29 +0000

    searchbar comp added

---
+++ src/components/context/SearchBar.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+import { ShopContext } from './ShopContext'
+
+const SearchBar = () => {
+    
+    const { search, setSearch, showSearch, setShowSearch } = useContext(ShopContext);
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SearchBar
