768b7afdcba5567b8e7da50b8db8232e4aa785a0
emess2g <essuonemma84gmail.com>
2024-10-23 09:04:00 +0000

    impl search func

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -4,7 +4,7 @@ import { assets } from '../assets/assets';
 import Title from '../components/Title';
 import ProductItem from '../components/context/ProductItem';
 const Collection = () => {
-  const { products } = useContext(ShopContext);
+  const { products, search, showSearch } = useContext(ShopContext);
   const [showFilter,setShowFilter] = useState(false);
   const [filterProducts,setFilterProducts] = useState([]);
   const [category,setCategory] = useState([]);
@@ -31,6 +31,10 @@ const Collection = () => {
 
   const applyFilter = () => {
      let productsCopy = products.slice();
+
+     if(search && showSearch){
+      productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+     }
 
      if(category.length > 0){
         productsCopy = productsCopy.filter(item => category.includes(item.category))
@@ -62,7 +66,7 @@ const Collection = () => {
 
   useEffect(()=> {
     applyFilter();
-  },[category,subCategory])
+  },[category,subCategory,search,showSearch])
 
   useEffect(()=>{
     sortProduct();
