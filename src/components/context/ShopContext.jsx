1aec160725d5ddd7cbd99a18b1707309ff7698f7
emess2g <essuonemma84gmail.com>
2024-10-25 10:49:29 +0000

    added cart count func

--- src/components/context/ShopContext.jsx
+++ src/components/context/ShopContext.jsx
@@ -33,10 +33,31 @@ const ShopContextProvider = (props) => {
         }
         setCartItems(cartData)
     }
+
+    const getCartCount = () => {
+        let totalCount = 0;
+
+        for(const items in cartItems){
+            for(const item in cartItems[items]){
+                try {
+                    if (cartItems[items][item] > 0) {
+                        totalCount += cartItems[items][item];
+                    }
+                } catch (error) {
+                    
+                }
+            }
+        }
+        return totalCount;
+    }
     
     useEffect(()=>{
        console.log(cartItems);     
     },[cartItems])
+
+
+
+
     const value = {
        products,
        currency,
