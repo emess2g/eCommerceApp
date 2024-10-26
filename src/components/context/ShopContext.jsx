a8d9974a866461ab935b7210e0bc111d28f78e31
emess2g <essuonemma84gmail.com>
2024-10-26 11:32:38 +0000

    place order navigate

--- src/components/context/ShopContext.jsx
+++ src/components/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../../assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext();
 
@@ -10,7 +11,8 @@ const ShopContextProvider = (props) => {
     const delivery_fee = 10;
     const [search,setSearch] = useState('');
     const [showSearch,setShowSearch] = useState(false);
-    const [cartItems,setCartItems] = useState({})
+    const [cartItems,setCartItems] = useState({});
+    const navigate = useNavigate();
 
     const addToCart = async(itemId,size) => {
 
@@ -82,7 +84,8 @@ const ShopContextProvider = (props) => {
        cartItems,addToCart,
        getCartCount,
        updateQuantity,
-       getCartAmount
+       getCartAmount,
+       navigate
     }
 
     return (
